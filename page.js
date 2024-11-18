d7ee47e67109b3ae4701694d50eba4e868015d6d
ArthurDelaporte <73790312+ArthurDelaporte@users.noreply.github.com>
2024-11-18 16:39:52 +0100

    Create page.js

---
+++ page.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Accordion from '@/Accordion';
+
+const Home = () => {
+  const accordionItems = [
+    {
+      header: 'Section 1',
+      content: 'Contenu de la section 1',
+    },
+    {
+      header: 'Section 2',
+      content: 'Contenu de la section 2',
+    },
+    {
+      header: 'Section 3',
+      content: 'Contenu de la section 3',
+    },
+  ];
+
+  return (
+      <div>
+        <h1>Accordéon Accessible</h1>
+        <Accordion items={accordionItems} />
+      </div>
+  );
+};
+
+export default Home;
